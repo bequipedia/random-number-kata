e9b09181a9a57fbb92b07e01ecb1c1613fcf1320
K Leonardo Ruiz Flórez <kleonardoruizflorez@gmail.com>
2023-05-31 10:07:01 +0200

    Added failing test for losing case

--- src/test/java/GuessingNumberGameShould.java
+++ src/test/java/GuessingNumberGameShould.java
@@ -3,6 +3,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
@@ -56,6 +59,7 @@ public class GuessingNumberGameShould {
 
         assertEquals("The number is higher", guessingMessage);
     }
+
     @Test
     void tell_player_number_is_higher_on_first_try_v2() {
         GuessingNumberGame guessingNumberGame = new GuessingNumberGame(randomNumberGenerator);
@@ -65,5 +69,27 @@ public class GuessingNumberGameShould {
         String guessingMessage = guessingNumberGame.guessNumber(firstTryNumber);
 
         assertEquals("The number is higher", guessingMessage);
+    }
+
+    @Test
+    void tell_player_loses() {
+        GuessingNumberGame guessingNumberGame = new GuessingNumberGame(randomNumberGenerator);
+        when(randomNumberGenerator.generateNumber()).thenReturn(5);
+
+        int firstTryNumber = 3;
+        int secondTryNumber = 8;
+        int thirdTryNumber = 6;
+
+        ArrayList<String>guessingMessages = new ArrayList<>(List.of(
+                guessingNumberGame.guessNumber(firstTryNumber),
+                guessingNumberGame.guessNumber(secondTryNumber),
+                guessingNumberGame.guessNumber(thirdTryNumber)
+        ));
+
+        assertEquals(new ArrayList<>(List.of(
+                "The number is higher",
+                "The number is lower",
+                "You lose"
+        )), guessingMessages);
     }
 }
